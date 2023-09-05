72f2a18aa33d873dc344737b45a0eb29253a23cf
Ruan Oliveira <ruanolisilva25@gmail.com>
2023-09-04 22:45:06 -0300

    Dio Basico

---
+++ src/MyClass.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String[] args) {
+       String firstName = "Ruan";
+       String secundName = "Oliveira";
+
+       String completName = completName ( firstName, secundName);
+        System.out.println(completName);
+    }
+
+    public static String completName (String firstName, String secundName){
+        return " Result " + firstName.concat(" ").concat(secundName);
+
+    }
+}
---
+++ src/studentReportCard.java
@@ -0,0 +1,15 @@
+public class studentReportCard {
+    public static void main(String[] args) {
+        int finalMedia = 6;
+        if(finalMedia < 6)
+            System.out.println(" REPROVED ");
+        else if (finalMedia == 6)
+            System.out.println(" MINERVA TEST ");
+        else
+            System.out.println(" APROVED !");{
+
+        }
+
+    }
+
+}
